e19b9e076fe4040c439f98a5372fd751ba80652c
Will Greenberg <greenberg.will@gmail.com>
2022-03-11 15:56:47 -0800

    add schedule and specialty properties to stylist model

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -10,6 +10,8 @@ namespace HairSalon.Models
     }
     public int StylistId { get; set; }
     public string Name { get; set; }
+    public string Specialty { get; set; }
+    public string Schedule { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
 }
